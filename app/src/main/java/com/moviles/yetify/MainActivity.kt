1bd91b297cb77ecb05eb85938c5d75840a5e91c9
javierdi12 <168677226+javierdi12@users.noreply.github.com>
2025-05-07 19:54:55 -0600

    feature/update-usertask (#1)
    * Update
    
    * Update task

--- app/src/main/java/com/moviles/yetify/MainActivity.kt
+++ app/src/main/java/com/moviles/yetify/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.moviles.yetify
-
+import androidx.activity.enableEdgeToEdge
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -21,6 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             YetifyTheme {
                 AppNavigation()
+
             }
         }
     }
