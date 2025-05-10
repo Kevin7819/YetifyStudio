e3e0f949cc879a25bdef773b65def1ac06932e53
javierdi12 <darlendiaz1212@gmail.com>
2025-05-10 03:02:05 -0600

    I was working on the animation user task view, view tasks

--- app/src/main/java/com/moviles/yetify/ui/theme/screens/MainMenuScreen.kt
+++ app/src/main/java/com/moviles/yetify/ui/theme/screens/MainMenuScreen.kt
@@ -1,7 +1,11 @@
-
 package com.moviles.yetify.ui.theme.screens
 
+
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,101 +15,264 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.*
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.moviles.yetify.R
+import kotlinx.coroutines.delay
+import kotlin.math.PI
+import kotlin.math.sin
+import kotlin.random.Random
 
 @Composable
 fun MainMenuScreen(navController: NavController) {
-    Column(
+    val density = LocalDensity.current
+
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
     ) {
-
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(240.dp) //
-                .background(Color(0xFF59C0EF)),
-            contentAlignment = Alignment.Center
-        ) {
-            Text(
-                text = "Gestión de Tareas",
-                color = Color.White,
-                fontSize = 28.sp,
-                fontWeight = FontWeight.Bold
-            )
-        }
-
-
-        Spacer(modifier = Modifier.height((-40).dp))
-
+        CloudBackgroundMain()
 
         Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 32.dp)
-                .fillMaxHeight(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            modifier = Modifier.fillMaxSize()
         ) {
-            CustomIconButton(
-                text = "Añadir nueva\ntarea",
-                icon = Icons.Default.Add,
-                iconPosition = IconPosition.RIGHT,
-                onClick = { navController.navigate("addTask") }
+            BoxWithConstraints(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(280.dp)
+                    .background(Color(0xFF59C0EF)),
+                contentAlignment = Alignment.Center
+            ) {
+                val density = LocalDensity.current
+                val maxPx = with(density) { maxWidth.toPx() }
+
+                val snowflakes = remember {
+                    List(25) {
+                        createSnowflake(maxPx)
+                    }
+                }
+
+                snowflakes.forEach { snowflake ->
+                    FallingSnowflake(snowflake)
+                }
+
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.yeti_normal),
+                        contentDescription = "Yeti mascota",
+                        modifier = Modifier
+                            .size(120.dp)
+                            .clip(CircleShape)
+                            .border(3.dp, Color.White, CircleShape)
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Text(
+                        text = "Tareas",
+                        color = Color.White,
+                        fontSize = 28.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height((-40).dp))
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 32.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                CustomIconButtonMain(
+                    text = "Añadir nueva\ntarea",
+                    icon = Icons.Default.Add,
+                    iconPositionMain = IconPositionMain.RIGHT,
+                    onClick = { navController.navigate("addTask") }
+                )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                CustomIconButtonMain(
+                    text = "Lista de\ntareas",
+                    icon = Icons.Default.List,
+                    iconPositionMain = IconPositionMain.RIGHT,
+                    onClick = { navController.navigate("taskList") }
+                )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                CustomIconButtonMain(
+                    text = "Calendario\nde tareas",
+                    icon = Icons.Default.DateRange,
+                    iconPositionMain = IconPositionMain.RIGHT,
+                    onClick = { navController.navigate("calendar") }
+                )
+
+                Spacer(modifier = Modifier.height(32.dp))
+
+                CustomIconButtonMain(
+                    text = "Volver",
+                    icon = Icons.Default.ArrowBack,
+                    iconPositionMain = IconPositionMain.LEFT,
+                    onClick = { navController.popBackStack() }
+                )
+            }
+        }
+    }
+}
+
+// Model glasses
+data class Snowflake(
+    val x: Float,
+    val size: Float,
+    val speed: Float,
+    val amplitude: Float,
+    val frequency: Float
+)
+
+@Composable
+fun FallingSnowflake(snowflake: Snowflake) {
+    var y by remember { mutableStateOf(-snowflake.size) }
+    var xOffset by remember { mutableStateOf(0f) }
+    val infiniteTransition = rememberInfiniteTransition()
+    val phase by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = (2 * PI).toFloat(),
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = (10000 / snowflake.frequency).toInt(),
+                easing = LinearEasing
+            )
+        )
+    )
+
+    val density = LocalDensity.current
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            y += snowflake.speed
+            xOffset = sin(phase * snowflake.frequency) * snowflake.amplitude
+            with(density) { if (y > 280.dp.toPx()) y = -snowflake.size }
+            delay(16)
+        }
+    }
+
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        drawCircle(
+            color = Color.White.copy(alpha = 0.8f),
+            radius = snowflake.size,
+            center = Offset(snowflake.x + xOffset, y),
+            style = Stroke(width = 1f)
+        )
+        drawCircle(
+            color = Color.White.copy(alpha = 0.5f),
+            radius = snowflake.size * 0.6f,
+            center = Offset(snowflake.x + xOffset, y),
+            style = Fill
+        )
+    }
+}
+
+fun createSnowflake(maxPx: Float): Snowflake {
+    val random = Random.Default
+    return Snowflake(
+        x = random.nextFloat() * maxPx,
+        size = random.nextFloat() * 8 + 4f,
+        speed = random.nextFloat() * 2 + 1,
+        amplitude = random.nextFloat() * 20 + 10,
+        frequency = random.nextFloat() * 0.5f + 0.3f
+    )
+}
+
+
+@Composable
+fun CloudBackgroundMain() {
+    val density = LocalDensity.current
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        with(density) {
+            val cloudColor = Color(0xFF59C0EF)
+            val cloudRadius = 50.dp.toPx()
+            val height = size.height
+            val width = size.width
+
+            // Cloud letf
+            drawCircle(
+                color = cloudColor,
+                radius = cloudRadius,
+                center = Offset(cloudRadius * 0.7f, height - cloudRadius * 0.4f)
+            )
+            drawCircle(
+                color = cloudColor,
+                radius = cloudRadius * 0.8f,
+                center = Offset(cloudRadius * 1.6f, height - cloudRadius * 0.6f)
+            )
+            drawCircle(
+                color = cloudColor,
+                radius = cloudRadius * 0.6f,
+                center = Offset(cloudRadius * 0.4f, height - cloudRadius * 1.2f)
+            )
+            drawCircle(
+                color = cloudColor,
+                radius = cloudRadius * 0.4f,
+                center = Offset(cloudRadius * 1.9f, height - cloudRadius * 1.0f)
             )
 
-            Spacer(modifier = Modifier.height(24.dp))
-
-            CustomIconButton(
-                text = "Lista de\ntareas",
-                icon = Icons.Default.List,
-                iconPosition = IconPosition.RIGHT,
-                onClick = { navController.navigate("taskList") }
+            // Cloud right inferior derecha
+            drawCircle(
+                color = cloudColor,
+                radius = cloudRadius,
+                center = Offset(width - cloudRadius * 0.7f, height - cloudRadius * 0.4f)
             )
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            CustomIconButton(
-                text = "Calendario\nde tareas",
-                icon = Icons.Default.DateRange,
-                iconPosition = IconPosition.RIGHT,
-                onClick = { navController.navigate("calendar") }
+            drawCircle(
+                color = cloudColor,
+                radius = cloudRadius * 0.8f,
+                center = Offset(width - cloudRadius * 1.6f, height - cloudRadius * 0.6f)
             )
-
-            Spacer(modifier = Modifier.height(32.dp))
-
-            CustomIconButton(
-                text = "Volver",
-                icon = Icons.Default.ArrowBack,
-                iconPosition = IconPosition.LEFT,
-                onClick = { navController.popBackStack() }
+            drawCircle(
+                color = cloudColor,
+                radius = cloudRadius * 0.6f,
+                center = Offset(width - cloudRadius * 0.4f, height - cloudRadius * 1.2f)
+            )
+            drawCircle(
+                color = cloudColor,
+                radius = cloudRadius * 0.4f,
+                center = Offset(width - cloudRadius * 1.9f, height - cloudRadius * 1.0f)
             )
         }
     }
 }
 
-
-
-enum class IconPosition {
+enum class IconPositionMain {
     LEFT, RIGHT
 }
 
 @Composable
-fun CustomIconButton(
+fun CustomIconButtonMain(
     text: String,
     icon: ImageVector,
-    iconPosition: IconPosition,
+    iconPositionMain: IconPositionMain,
     onClick: () -> Unit
 ) {
     Button(
@@ -128,7 +295,7 @@ fun CustomIconButton(
             horizontalArrangement = Arrangement.Center,
             modifier = Modifier.fillMaxWidth()
         ) {
-            if (iconPosition == IconPosition.LEFT) {
+            if (iconPositionMain == IconPositionMain.LEFT) {
                 Icon(
                     imageVector = icon,
                     contentDescription = null,
@@ -144,7 +311,7 @@ fun CustomIconButton(
                 textAlign = TextAlign.Center
             )
 
-            if (iconPosition == IconPosition.RIGHT) {
+            if (iconPositionMain == IconPositionMain.RIGHT) {
                 Spacer(modifier = Modifier.width(8.dp))
                 Icon(
                     imageVector = icon,
@@ -154,4 +321,10 @@ fun CustomIconButton(
             }
         }
     }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun MainMenuScreenPreview() {
+    MainMenuScreen(navController = rememberNavController())
 }
